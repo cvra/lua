c604bd0ec9d90610affc276d84ac6d2ca7d16527
Antoine Albertelli <antoinea101@gmail.com>
2014-03-28 21:50:48 +0100

    REPL now shows expression value as feedback.

--- test_lua.c
+++ test_lua.c
@@ -7,6 +7,14 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
+static int test_func(lua_State *l)
+{
+
+    lua_pushinteger(l, strlen(lua_tostring(l, -1)));
+    return 1;
+}
+
+
 int main(void)
 {
 
@@ -16,23 +24,40 @@ int main(void)
 	l = luaL_newstate();
     luaL_openlibs(l);
 
+    lua_pushcfunction(l, test_func);
+    lua_setglobal(l, "mylen");
+
 	/* You can do what you want here. Note: Remember to update the libraries used (see above) */
 	/* if you add to your program and use new Lua libraries. */
 	/* In the lines below, I load and run the Lua code contained in the file */
 	/* "script.lua". */
 	/* Plus print some text directly from C. */
-    char *command[100];
-	printf("This line in directly from C\n\n");
+    char command[100];
+    char real_command[100];
+    int ret;
 
     while(1) {
         printf(">> ");
-        if (gets(command) == NULL)
-            break;
-        luaL_loadstring(l, command);
-        lua_pcall(l, 0, 0, 0);
-    }
 
-	printf("\nBack to C again\n\n");
+        if (fgets(command, 100, stdin) == NULL)
+            break;
+
+        sprintf(real_command, "print(%s)", command);
+
+        luaL_loadstring(l, real_command);
+        ret = lua_pcall(l, 0, LUA_MULTRET, 0);
+        if (ret) {
+            lua_pop(l, 1);
+            luaL_loadstring(l, command);
+            ret = lua_pcall(l, 0, LUA_MULTRET, 0);
+            if (ret) {
+                printf("ERROR %d : %s\n", ret, lua_tostring(l, -1));
+                lua_pop(l, 1);
+            }
+        }
+
+        printf("top of stack : %d\n", lua_gettop(l));
+    }
 
 	/* Remember to destroy the Lua State */
 	lua_close(l);
